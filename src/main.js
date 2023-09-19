dbc5e8e85a5b158a31242e902993f863c7e424f8
Nemanja Ratic <ratic02.r@gmail.com>
2023-09-19 20:29:24 +0200

    v-preload pokusaj

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+Vue.directive('preload', {
+    inserted: function (el) {
+        const image = new Image();
+        image.src = el.src;
+    }
+});
 const app = createApp(App)
 
 app.use(createPinia())
